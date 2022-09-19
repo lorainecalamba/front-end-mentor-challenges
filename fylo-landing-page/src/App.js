238c3ca500fc6d7a03e4eb8b08b5b4980177a326
lorainecalamba <loraine902@gmail.com>
2022-09-19 19:47:23 +0800

    Added productivity component to App.js

--- fylo-landing-page/src/App.js
+++ fylo-landing-page/src/App.js
@@ -3,14 +3,14 @@ import Footer from "./components/Footer/Footer";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Intro from "./components/Intro/Intro";
 import Navbar from "./components/Navbar/Navbar";
+import Productivity from "./components/Productivity/Productivity";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-
       <Intro />
-
+      <Productivity />
       <GetStarted />
       <Footer />
     </React.Fragment>
